089c6039820f5f91aebece9b3d08f103f58988a8
신동주 <sdj3701@naver.com>
2025-12-02 16:47:05 +0900

    Fix : 테스트 함수 내에서 정의 가능

--- RC Car/Assets/Generated/BlocksGenerated.cs
+++ RC Car/Assets/Generated/BlocksGenerated.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using UnityEngine;
 public class BlocksGenerated : MonoBehaviour
 {
-    public void a(object a)
+    public void Speed()
     {
+            var stop = 0f;
+            var go = 200f;
+            var turn = 150f;
     }
     public void Run()
     {
+        Speed();
 
     }
 }
